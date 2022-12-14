66015f6034e58d75b3164adb1bcd772c5ada9c84
ConnorHoare <connor@tipcigroup.com>
2022-12-14 17:38:24 +0000

    update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const Nav = () => {
     return (
             <nav class="main-nav">
                 <ul>
-                    <li><NavLink to="cats">Cats</NavLink></li>
-                    <li><NavLink to="dogs">Dogs</NavLink></li>
-                    <li><NavLink to="computer">Computers</NavLink></li>
+                    <li><Link to="/cats">Cats</Link></li>
+                    <li><Link to="/dogs">Dogs</Link></li>
+                    <li><Link to="/computer">Computers</Link></li>
                 </ul>
             </nav> 
     )
